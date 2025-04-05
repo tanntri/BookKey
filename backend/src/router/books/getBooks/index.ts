573a48f3994efb4c39229f342142b9660afb58ad
tanntri <tann.triyapanjawit@gmail.com>
2025-04-05 13:58:46 -0700

    adding cursor to database call

--- backend/src/router/books/getBooks/index.ts
+++ backend/src/router/books/getBooks/index.ts
@@ -1,7 +1,8 @@
 import _ from 'lodash';
 import { trpc } from '../../../lib/trpc';
+import { zGetBooksTrpcInput } from './input';
 
-export const getBooksTrpcRoute = trpc.procedure.query(async ({ctx}) => {
+export const getBooksTrpcRoute = trpc.procedure.input(zGetBooksTrpcInput).query(async ({ctx, input}) => {
         const books = await ctx.prisma.book.findMany({
             select: {
                 id: true,
@@ -9,11 +10,21 @@ export const getBooksTrpcRoute = trpc.procedure.query(async ({ctx}) => {
                 book: true,
                 author: true,
                 description: true,
-                createdAt: true
+                createdAt: true,
+                serialNumber: true
             },
-            orderBy: {
+            orderBy: [{
                 createdAt: 'desc'
-            }
+            }, {
+                serialNumber: 'desc'
+            }],
+            cursor: input.cursor ? {serialNumber: input.cursor} : undefined,
+            take: input.limit + 1
         })
-        return { books }
+
+        const nextBook = books.at(input.limit);
+        const nextCursor = nextBook?.serialNumber;
+        const bookExceptNext = books.slice(0, input.limit);
+
+        return { books: bookExceptNext, nextCursor }
     })
