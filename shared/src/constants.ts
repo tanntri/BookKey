8dc12f3e0e7fa6e69dc8db8de25afbc2e68271e8
tanntri <tann.triyapanjawit@gmail.com>
2025-05-27 16:20:04 -0700

    Add Constants

---
+++ shared/src/constants.ts
@@ -0,0 +1,82 @@
+export const RES = {
+    bookCategories: {
+        'Arts': [
+            'Architecture',
+            'Art History',
+            'Fashion',
+            'Film',
+            'Music',
+            'Photography',
+        ],
+        'Fiction': [
+            'Fantasy',
+            'Historical Fiction',
+            'Horror',
+            'Literature',
+            'Mystery',
+            'Science Fiction',
+            'Thriller'
+        ],
+        'Science & Mathematics': [
+            'Biology',
+            'Chemistry',
+            'Mathematics',
+            'Physics',
+            'Programming'
+        ],
+        'Business & Finance': [
+            'Management',
+            'Entrepreneurship',
+            'Business Economics',
+            'Business Success'
+        ],
+        'History': [
+            'Ancient Civilization',
+            'Archaeology',
+            'Anthropology',
+            'World War II',
+        ],
+        'Health & Wellness': [
+            'Cooking',
+            'Mental Health',
+            'Exercise',
+            'Nutrition',
+            'Self-help'
+        ],
+        'Biography': [
+            'Religion',
+            'Political Science',
+            'Psychology'
+        ],
+        'Textbooks': [
+            'History',
+            'Geography',
+            'Algebra',
+            'Education',
+            'Business & Economics',
+            'Science',
+            'Computer Science'
+        ]
+
+    },
+    common: {
+        bookkey: "BookKey",
+        noCover: "No Cover",
+        authors: "Authors"
+    },
+    profile: {
+        userProfile: (username: string) => `${username}'s Profile`,
+        profilePage: "Profile Page",
+        library: "Library",
+        bookmarks: "Bookmarks",
+        read: "Read",
+        reviews: "Reviews"
+    },
+    editProfile: {
+        editProfile: "Edit Profile",
+        profileUpdated: "Profile Updated",
+        passwordUpdated: "Password Updated",
+        updateProfile: "Update Profile",
+        updatePassword: "Update Password"
+    }
+}
